83db84c9cd22c2621b1be61bef9f0dea8f5c92a8
dgmar <dgmar@DESKTOP-OVV763L>
2021-03-24 11:25:45 +0100

    Primero

---
+++ src/examen/EXAMEN_24M_PRIMO.java
@@ -0,0 +1,15 @@
+package examen;
+
+import java.util.Scanner;
+
+public class EXAMEN_24M_PRIMO {
+	public static void main(String arg[]) {
+		Scanner ent = new Scanner(System.in);
+		int num=0;
+		
+		System.out.println("Buenos días Ceinmark, soy Mario Diaz");
+		System.out.println("Introduce un número para averiguar si es primo o no:");
+		num=ent.nextInt();
+		
+	}
+}
