c580c14c68c537d076bcedb40e315a51041db1fa
dgmar <dgmar@DESKTOP-OVV763L>
2021-03-24 11:45:25 +0100

    Rama1

--- src/examen/EXAMEN_24M_PRIMO.java
+++ src/examen/EXAMEN_24M_PRIMO.java
@@ -11,5 +11,14 @@ public class EXAMEN_24M_PRIMO {
 		System.out.println("Introduce un número para averiguar si es primo o no:");
 		num=ent.nextInt();
 		
+		if((num%2)==1 || (num%3)==1||(num%5)==1) {
+			
+			System.out.println("no es primo");
+			System.out.println(num+"%2="+(num%2));
+			System.out.println(num+"%3="+(num%3));
+			System.out.println(num+"%5="+(num%5));
+			
+		}
+		
 	}
 }
